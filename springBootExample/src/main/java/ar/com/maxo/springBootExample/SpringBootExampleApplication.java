420c55cf50cfc848c37ec2707598fdae2c59f040
Maximiliano Torchio <max3zet@gmail.com>
2019-06-29 21:15:35 -0300

    Initial commit. SpringBoot initializer app template, with Loombok and Spring Session.

---
+++ springBootExample/src/main/java/ar/com/maxo/springBootExample/SpringBootExampleApplication.java
@@ -0,0 +1,13 @@
+package ar.com.maxo.springBootExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExampleApplication.class, args);
+	}
+
+}
