dd774f853f615bb86392f2325e99375dfad88b6b
Maximiliano Torchio <max3zet@gmail.com>
2019-06-29 22:05:54 -0300

    App basica, con Log4J.

--- springBootExample/src/main/java/ar/com/maxo/springBootExample/SpringBootExampleApplication.java
+++ springBootExample/src/main/java/ar/com/maxo/springBootExample/SpringBootExampleApplication.java
@@ -1,13 +1,21 @@
 package ar.com.maxo.springBootExample;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootExampleApplication {
 
+	private static final Logger logger = LogManager.getLogger(SpringBootExampleApplication.class);
+
 	public static void main(String[] args) {
+		logger.info(">>> Starting app...");
 		SpringApplication.run(SpringBootExampleApplication.class, args);
 	}
 
 }
+
+
+
