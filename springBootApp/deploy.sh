724b6bf753a9890a77adab892b4a710d0a32c594
Maximiliano Torchio <max3zet@gmail.com>
2019-07-06 02:33:39 -0300

    Se modifica cuando instalo los componentes para java y gae

--- springBootApp/deploy.sh
+++ springBootApp/deploy.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-set -x  #Habilito modo verbose.
-echo 'Me autentico a GAE'
-gcloud auth activate-service-account --project benita-accesorios --key-file=keyfile.json
-echo 'Installo el componente app-engine-java'
-gcloud components install app-engine-java
-echo 'Ejecuto deploy con mvnw...'
+echo '>>>>> Me autentico a GAE'
+./google-cloud-sdk/bin/gcloud auth activate-service-account --project benita-accesorios --key-file=keyfile.json
+echo '>>>>> Ejecuto deploy con mvnw...'
 ./mvnw -DskipTests appengine:deploy
