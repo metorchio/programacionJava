879a321cfdd538444c4fbc453324dda56ab107ea
Maximiliano Torchio <max3zet@gmail.com>
2019-07-06 03:00:12 -0300

    Se intenta deploy manual con script

--- springBootApp/deploy.sh
+++ springBootApp/deploy.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
+echo '>>>>> Instalo Google Cloud SDK'
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+apt-get install apt-transport-https ca-certificates
+echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+apt-get update && apt-get install google-cloud-sdk -y
+apt-get install google-cloud-sdk-app-engine-java -y
+
 echo '>>>>> Me autentico a GAE'
-./google-cloud-sdk/bin/gcloud auth activate-service-account --project benita-accesorios --key-file=keyfile.json
+gcloud auth activate-service-account --project benita-accesorios --key-file=keyfile.json
 echo '>>>>> Ejecuto deploy con mvnw...'
 ./mvnw -DskipTests appengine:deploy
