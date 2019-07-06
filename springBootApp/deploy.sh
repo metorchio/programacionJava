0f4673ae3a71a04b8c3f0990ea65b1ce93281452
Maximiliano Torchio <max3zet@gmail.com>
2019-07-06 02:22:40 -0300

    Se hace verbose el script y se agregan logs.

--- springBootApp/deploy.sh
+++ springBootApp/deploy.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+set -x  #Habilito modo verbose.
+echo 'Me autentico a GAE'
 gcloud auth activate-service-account --project benita-accesorios --key-file=keyfile.json
+echo 'Installo el componente app-engine-java'
 gcloud components install app-engine-java
+echo 'Ejecuto deploy con mvnw...'
 ./mvnw -DskipTests appengine:deploy
