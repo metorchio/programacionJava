95a2c2de15ed1f53b970930f1054e5d600a7757e
Maximiliano Torchio <max3zet@gmail.com>
2019-07-06 03:10:09 -0300

    Se doy sudo al script.

--- springBootApp/deploy.sh
+++ springBootApp/deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 echo '>>>>> Instalo Google Cloud SDK'
-echo "sudo deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 sudo apt-get install apt-transport-https ca-certificates
-echo "sudo deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 sudo apt-get update && sudo apt-get install google-cloud-sdk -y
 sudo apt-get install google-cloud-sdk-app-engine-java -y
