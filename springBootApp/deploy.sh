432aa6bb64d9d2e519e136bbc7fed972388dc445
Maximiliano Torchio <max3zet@gmail.com>
2019-07-06 03:04:23 -0300

    Se doy sudo al script.

--- springBootApp/deploy.sh
+++ springBootApp/deploy.sh
@@ -3,11 +3,10 @@
 echo '>>>>> Instalo Google Cloud SDK'
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 apt-get install apt-transport-https ca-certificates
-echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-apt-get update && apt-get install google-cloud-sdk -y
+echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+apt-get update && sudo apt-get install google-cloud-sdk -y
 apt-get install google-cloud-sdk-app-engine-java -y
-
 echo '>>>>> Me autentico a GAE'
 gcloud auth activate-service-account --project benita-accesorios --key-file=keyfile.json
 echo '>>>>> Ejecuto deploy con mvnw...'
