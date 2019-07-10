f9e25bbcd432f61f0b426f257a2035210fcc3cba
Maximiliano Torchio <max3zet@gmail.com>
2019-07-09 21:24:39 -0300

    Se loggean parametros del ctx.

--- springBootExample/src/main/java/ar/com/maxo/springBootExample/SpringBootExampleApplication.java
+++ springBootExample/src/main/java/ar/com/maxo/springBootExample/SpringBootExampleApplication.java
@@ -24,8 +24,10 @@ public class SpringBootExampleApplication {
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
 
+			System.out.println("Startup Date: " + ctx.getStartupDate() );
+			System.out.println("Environment: " + ctx.getEnvironment() );
+			System.out.println("Class loader: " + ctx.getClassLoader() );
 			System.out.println("Let's inspect the beans provided by Spring Boot:");
-
 			String[] beanNames = ctx.getBeanDefinitionNames();
 			Arrays.sort(beanNames);
 			for (String beanName : beanNames) {
