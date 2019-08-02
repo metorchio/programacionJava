7c2616c6f66993d3ad51a16bf4f495850c8edcff
Maxo..# <max3zet@gmail.com>
2019-08-01 21:41:48 -0300

    Se actualiza el puerto indicado por pantalla al levantar publisher del servicio

--- vanilla/java/webServiceJava/src/main/java/ar/com/maxo/endpoint/MyServicePublisher.java
+++ vanilla/java/webServiceJava/src/main/java/ar/com/maxo/endpoint/MyServicePublisher.java
@@ -13,7 +13,7 @@ public class MyServicePublisher {
 
     public static void main(String[] args) {
         System.out.println("Escuchando en 'localhost:9999'...");
-        System.out.println("Consultar WSDL en 'http://localhost:9999/ws/hello?wsdl'...");
+        System.out.println("Consultar WSDL en 'http://localhost:8080/ws/hello?wsdl'...");
         Endpoint.publish("http://localhost:8080/ws/hello", new MyServiceImpl() );
     }
 
